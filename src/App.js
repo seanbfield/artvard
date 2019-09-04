4f528db8231b79d96d68ba638bbbd87918d7b142
rhbfield <rhblanchfield@programmer.net>
2019-09-04 17:44:16 +0200

    api response

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
-import React from 'react';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 import { Route, Switch } from 'react-router-dom';
+import './App.css';
+
+import {
+  fetchAllObjects
+}
+from './api-helper/services'
 import Home from './components/Home'
 import Results from './components/Results'
 import Details from './components/Details'
 
+
 function App() {
+  const [data, setData] = useState({ hits: [] });
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetchAllObjects();
+      setData(result.data);
+    };
+    fetchData();
+  },[])
+  
+
+
   return (
+    <>
+
+    <ul>
+      {data.hits.map(item => (
+        <li key={item.objectID}>
+          <p>{item.person}</p>
+        </li>
+      ))}
+    </ul>
+
+
+
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/results" component={Results} />
         <Route exact path="/details" component={Details} />
       </Switch>
-    </div>
+      </div>
+      </>
   );
 }
 
--- src/api-helper/services.js
+++ src/api-helper/services.js
@@ -1,4 +1,16 @@
 
 import axios from 'axios'
 
-const token = '08934a20-b202-11e9-95eb-6525f67b00a0'
+const TOKEN = '08934a20-b202-11e9-95eb-6525f67b00a0'
+const BASE_URL = 'https://api.harvardartmuseums.org'
+
+const api = axios.create({
+ baseURL: BASE_URL,
+})
+
+
+export const fetchAllObjects = async () => {
+ const response = await api.get(`/object?apikey=${TOKEN}`)
+ console.log(response);
+ return response
+}
