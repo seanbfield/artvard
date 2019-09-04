f00611af596f6948385c0ca5b7119434990536f2
rhbfield <rhblanchfield@programmer.net>
2019-09-04 18:24:41 +0200

    moved list to Home.jsx component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,16 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react'
 import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
-import {
-  fetchAllObjects
-}
-from './api-helper/services'
 import Home from './components/Home'
 import Results from './components/Results'
 import Details from './components/Details'
 
 
 function App() {
-  const [data, setData] = useState({
-    records: []
-  });
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetchAllObjects();
-      setData(result);
-    };
-    fetchData();
-  },[])
-  
-
-
   return (
     <>
-
-    <ul>
-        {data.records.map(record => (
-        <li key={record.id}>
-            <a href={record.url}>{record.displayname}</a>
-        </li>
-      ))}
-    </ul>
-
-
-
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import {
+ fetchAllObjects
+}
+ from '../api-helper/services'
+
+
 
 export default function Home() {
+ const [data, setData] = useState({
+  records: []
+ });
+
+ useEffect(() => {
+  const fetchData = async () => {
+   const result = await fetchAllObjects();
+   setData(result);
+  };
+  fetchData();
+ }, [])
+
  return (
-  <div>
-   <h1>Home</h1>
-  </div>
+  <>
+   <ul>
+    {data.records.map(record => (
+     <li key={record.id}>
+      <a href={record.url} target="_blank">{record.displayname}</a>
+     </li>
+    ))}
+   </ul>
+
+  </>
  )
 }
