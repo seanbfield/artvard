9df0b4cf7cb8180d335ebedf305527f53df96646
seanbfield <rhblanchfield@programmer.net>
2019-09-06 14:04:25 +0200

    css updates

--- src/api-helper/services.js
+++ src/api-helper/services.js
@@ -5,19 +5,19 @@ const TOKEN = '08934a20-b202-11e9-95eb-6525f67b00a0'
 const BASE_URL = 'https://api.harvardartmuseums.org'
 
 const api = axios.create({
- baseURL: BASE_URL,
+  baseURL: BASE_URL,
 })
 
 
 export const fetchAllObjects = async () => {
- const response = await api.get(`/object?apikey=${TOKEN}`)
- console.log(response.data);
- return response.data
+  const response = await api.get(`/object?apikey=${TOKEN}&size=50&page=500`)
+  console.log(response.data);
+  return response.data
 }
 
 
 export const fetchImages = async () => {
- const response = await api.get(`/image?apikey=${TOKEN}`)
- console.log(response.data);
- return response.data
+  const response = await api.get(`/image?apikey=${TOKEN}`)
+  console.log(response.data);
+  return response.data
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,87 +1,85 @@
 import React, { useState, useEffect } from 'react';
 import {
- fetchAllObjects,
- fetchImages
+  fetchAllObjects,
+  fetchImages
 }
- from '../api-helper/services'
+  from '../api-helper/services'
 
-import StackGrid, { transitions, easings }  from "react-stack-grid";
+import StackGrid, { transitions, easings } from "react-stack-grid";
 import Header from './Header';
 const transition = transitions.scaleDown;
 
 
 
 export default function Home() {
- const [data, setData] = useState({ records: [] });
- 
- const [img, setImageData] = useState({ records: []})
+  const [data, setData] = useState({ records: [] });
+
+  const [img, setImageData] = useState({ records: [] })
 
 
 
- useEffect(() => {
-  const fetchData = async () => {
-   const result = await fetchAllObjects();
-   setData(result);
-  };
-  fetchData();
- }, [])
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetchAllObjects();
+      setData(result);
+    };
+    fetchData();
+  }, [])
 
 
- useEffect(() => {
-  const fetchImg = async () => {
-   const result = await fetchImages();
-   setImageData(result);
-  };
-  fetchImg();
- }, [])
+  useEffect(() => {
+    const fetchImg = async () => {
+      const result = await fetchImages();
+      setImageData(result);
+    };
+    fetchImg();
+  }, [])
 
 
 
 
 
- return (
-  <>
-   <Header />
+  return (
+    <>
+      <Header />
+      {/* <div className="container mt-5"> */}
+      <div className="image">
+        <StackGrid
+          monitorImagesLoaded
+          columnWidth={200}
+          duration={600}
+          gutterWidth={15}
+          gutterHeight={15}
+          easing={easings.cubicOut}
+          appearDelay={60}
+          appear={transition.appear}
+          appeared={transition.appeared}
+          enter={transition.enter}
+          entered={transition.entered}
+          leaved={transition.leaved}
+        >
 
-    
-   <div className="container mt-5">
-   <div className="image">
-   <StackGrid
-    monitorImagesLoaded
-    columnWidth={200}
-    duration={600}
-    gutterWidth={20}
-    gutterHeight={15}
-    easing={easings.cubicOut}
-    appearDelay={60}
-    appear={transition.appear}
-    appeared={transition.appeared}
-    enter={transition.enter}
-    entered={transition.entered}
-    leaved={transition.leaved}
-   >
-   
-    {data.records.map(record => (
-     <>
-     <div key={record.id}>
-       <h3>{record.title}</h3>
-       <br />
-       {record.culture}
-       <br />
-       {record.period}
-       <br />
-       {record.medium}
-       <br />
-       {record.yearmade}
-       <img src={record.primaryimageurl} width="200" alt="artist" />
-      <a href={record.url} target="_blank">More Info</a>
+          {data.records.map(record => (
+            <>
+              <div key={record.id}>
+                <h3>{record.title}</h3>
+                <br />
+                {record.culture}
+                <br />
+                {record.period}
+                <br />
+                {record.medium}
+                <br />
+                {record.yearmade}
+                <img src={record.primaryimageurl} width="200" alt="artist" />
+                <a href={record.url} target="_blank">More Info</a>
+              </div>
+            </>
+          ))}
+        </StackGrid>
       </div>
-     </>
-    ))}
-   </StackGrid>
-    </div>
-     </div>
- 
-  </>
- )
+      {/* </div> */}
+
+    </>
+  )
 }
