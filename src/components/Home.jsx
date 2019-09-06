f906b3bbddc10da35cc334c09b05e4bd78b69fbf
rhbfield <rhblanchfield@programmer.net>
2019-09-06 12:25:39 +0200

    css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,9 @@ import {
 }
  from '../api-helper/services'
 
+import StackGrid, { transitions }  from "react-stack-grid";
+const { scaleDown, fadeDown } = transitions;
+
 
 
 export default function Home() {
@@ -37,11 +40,18 @@ export default function Home() {
 
  return (
   <>
+   <StackGrid
+    appear={fadeDown.appear}
+    appeared={fadeDown.appeared}
+    enter={scaleDown.enter}
+    entered={scaleDown.entered}
+    leaved={scaleDown.leaved}
+    columnWidth={200}>
    <div>
     {data.records.map(record => (
     <>
      <div key={record.id}>
-       <p>{record.title}</p>
+       <h3>{record.title}</h3>
        <br />
        {record.culture}
        <br />
@@ -52,10 +62,11 @@ export default function Home() {
        {record.yearmade}
        <img src={record.primaryimageurl} width="200" alt="artist" />
       <a href={record.url} target="_blank">More Info</a>
-     </div>
+      </div>
      </>
     ))}
-   </div>
+    </div>
+   </StackGrid>
   </>
  )
 }
